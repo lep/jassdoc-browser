317580ab8f4ce02af73cfd144beb6871914b34a9
lep <lep.delete@googlemail.com>
2019-10-12 17:12:16 +0200

    Show params of function even if they have no documentation

--- server.rb
+++ server.rb
@@ -19,26 +19,26 @@ get '/doc/:fn' do |fn|
     comment = $db.execute "select comment from comments where fnname == ?", fn
     comment = comment[0][0] rescue "No Description yet"
 
-    #parameters = $db.execute "select param, value from parameters where fnname == ?", fn
-    #parameters = $db.execute "select param, value from parameters natural left join params_by_index where fnname == ? AND parameters.param == params_by_index.argname ORDER BY idx", fn
     query = <<SQL
-        SELECT P.param, P.value, T.value
-	FROM parameters AS P
+select Ty.param, Ty.value, Doc.value
+from
+( select Value, param
+  from Params_extra
+  where Anname == 'param_order' AND fnname == ?
+) as Ord
 
-	INNER JOIN
-	( SELECT param, value
-          FROM params_extra
-	  WHERE anname == 'param_order' AND fnname == ?
-	) AS Ord ON P.param == Ord.param
+inner join
+    ( select param, value
+    from params_extra
+    where anname == 'param_type' and fnname == ?
+    ) as Ty on Ty.param == Ord.param
 
-	INNER JOIN
-	( SELECT param, value
-	  FROM params_extra
-	  WHERE anname == 'param_type' AND fnname == ?
-	) AS T ON P.param == T.param
+left outer join
+    ( select param, value from parameters
+      where fnname == ?
+    ) as Doc on Doc.param == Ord.param
 
-	WHERE fnname == ?
-	ORDER BY Ord.value
+order by Ord.value
 SQL
     parameters = $db.execute query, [fn, fn, fn]
     annotations = $db.execute "select anname, value from annotations where fnname == ? order by anname", fn
