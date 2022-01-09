96632b396566b123997702ae8730484202e09e78
lep <lep.delete@googlemail.com>
2022-01-09 16:03:49 +0100

    Differentiate bj_globals and cj_globals.

--- mkregex.sql
+++ mkregex.sql
@@ -1,7 +1,11 @@
 
-select "var globals = /^(?:" || group_concat(fnname, "|") || ")\b/"
+select "var bj_globals = /^(?:" || group_concat(fnname, "|") || ")\b/"
 from annotations
-where anname == 'type' and value == 'global';
+where anname == 'type' and value == 'global' and fnname like "bj_%";
+
+select "var cj_globals = /^(?:" || group_concat(fnname, "|") || ")\b/"
+from annotations
+where anname == 'type' and value == 'global' and fnname not like "bj_%";
 
 select "var types = /^(?:" || group_concat(fnname, "|") || ")\b/"
 from annotations
